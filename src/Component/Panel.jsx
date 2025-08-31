285d20a848b09c47653d85a86b4557f39240d8c8
TakeMichiks <104736691+TakeMichiks@users.noreply.github.com>
2025-08-30 22:28:00 -0400

    Cambios en el backend y fronten dpara el guardado automatico de los cambios

--- src/Component/Panel.jsx
+++ src/Component/Panel.jsx
@@ -1,30 +1,55 @@
 import React from 'react';
 import styles from "./stylesPanel.module.css";
-import ImageUploader from './ImageUploader'; // Importa el nuevo componente
-
+import { LuLayoutPanelLeft } from "react-icons/lu";
+import { AiOutlineUser } from "react-icons/ai";
+import { AiOutlineHome } from "react-icons/ai";
+import { LiaChalkboardTeacherSolid } from "react-icons/lia";
+import { PiUserCircleGearThin } from "react-icons/pi";
 function Panel({ onClickUploader, onClickCerrarSecion}) {
 
   return (
     <>
-      <div className={styles.Content}>
+      <div class="hover:no-underline flex justify-center bg-black rounded-2xl p-5 w-full h-full items-center">
         <nav>
-          <ul>
-            <li><a href="#">inicio</a></li>
-            <li><a href="#">Panel</a></li>
-            <li><a href="#">Estudiantes</a></li>
-            <li><a href="#">Profesores</a></li>
-            <li><a href="#">Ajustes</a></li>
+          <ul class="content-between flex flex-col gap-1">
+            <li class=" flex gap-2 justify-center w-37 h-9 rounded-sm hover:border-1 transition-all hover:bg-white hover:text-black ">
+              <AiOutlineHome class="flex justify-center mt-1.5" />
+              <a href="#" class="font-bold mt-0.5">
+                Inicio
+              </a>
+            </li>
+            <li class=" flex gap-2 justify-center w-37 h-9 rounded-sm hover:border-1 transition-all hover:bg-white hover:text-black ">
+              <LuLayoutPanelLeft class="flex justify-center mt-1.5" />
+              <a href="#">Panel</a>
+            </li>
+            <li class="  flex gap-2 justify-center w-37 h-9 rounded-sm hover:border-1 transition-all hover:bg-white hover:text-black ">
+              <AiOutlineUser class="flex justify-center mt-1.5" />
+              <a href="#" class="font-bold mt-0.5">
+                Estudiantes
+              </a>
+            </li>
+            <li class="  flex gap-2 justify-center w-37 h-9 rounded-sm hover:border-1 transition-all hover:bg-white hover:text-black ">
+              <LiaChalkboardTeacherSolid class="flex justify-center mt-1.5" />
+              <a href="#" class="font-bold mt-0.5">Profesores</a>
+            </li>
+            <li class="  flex gap-2 justify-center w-37 h-9 rounded-sm hover:border-1  transition-all hover:bg-white hover:text-black ">
+              <PiUserCircleGearThin class="flex justify-center mt-1.5" />
+              <a href="#" class="font-bold mt-0.5">Ajustes</a>
+            </li>
           </ul>
         </nav>
       </div>
 
       {/* Renderiza el componente de subida de imágenes aquí */}
-    <button onClick={onClickUploader}>
-      Haz clic aquís
-      </button>
+      <button onClick={onClickUploader}>Haz clic aquís</button>
 
       <div>
-        <button onClick={onClickCerrarSecion}>Cerrar Sesion</button>
+        <button
+          onClick={onClickCerrarSecion}
+          className="flex items-center gap-2" // Usa estas clases de Tailwind
+        >
+          <span>Sesión</span>
+        </button>
       </div>
     </>
   );
